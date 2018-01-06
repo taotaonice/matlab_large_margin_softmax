193e78a84fb375af90c7af9924e039adf4acefc3
taotao <335702517@qq.com>
2018-01-07 00:35:44 +0800

    done

--- test.m
+++ test.m
@@ -6,37 +6,29 @@ caffe.reset_all
 caffe.set_mode_gpu
 caffe.set_device(0)
 
-
 load('mnist.mat');
 load('w.mat');
 
 test_img = (test_img-0.5).*2;
 test_img = reshape(test_img, 28, 28, []);
 
-solver = caffe.Solver('ipsolver.prototxt');
-solver.net.copy_from('tmp.caffemodel');
+net = caffe.Net('test.prototxt', 'train');
+net.copy_from('tmp.caffemodel');
 
-solver.net.reshape_as_input({zeros(28,28,1,1)});
+net.reshape_as_input({zeros(28,28,1,1)});
 test_size = 10000;
 right = 0;
 wnorm = sqrt(sum(w.^2, 1)); % 1x10
 for ii = 1:test_size
-    solver.net.forward({test_img(:, :, ii)});
+    net.forward({test_img(:, :, ii)});
     label = test_label(ii);
-    x = solver.net.blobs('relu3').get_data();
+    x = net.blobs('bn_ip').get_data();
     xnorm = sqrt(sum(x.^2, 1)); % 1xn
-    theta = (w'*x)./(wnorm'*xnorm); % 10xn
-    k = floor(theta.*2./pi); % 10xn
+    theta = acos((w'*x)./(wnorm'*xnorm)); % 10xn
     
-    f = (-1).^k.*2.*(w'*x).^2./(wnorm'*xnorm) - ...
-        (2.*k+(-1).^k).*(wnorm'*xnorm); % 10xn
+    f = wnorm'*xnorm.*fi(theta, 2);
     
-    s = sum(exp(w'*x), 1);
-    s = repmat(s, 10, 1); % 10xn
-    s = s - exp(w'*x);
-    loss = exp(f)./(exp(f) + s); % 10xn
-    
-    [~, ind] = max(loss);
+    [~, ind] = max(f);
     if ind == label+1
         right = right+1;
     end
